7ed276dd856e2259ce03ad540c02888988a1d31d
Karimtarek011420 <karimtarek011420@gmail.com>
2024-12-16 20:48:14 +0200

    Your commit message

---
+++ src/app/_Compontents/CountryHeader/CountryHeader.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import germany from "../../../assets/images/germany.svg";
+import uk from "../../../assets/images/united-k.svg";
+import china from "../../../assets/images/china.svg";
+import france from "../../../assets/images/france.svg";
+import arrow from "../../../assets/images/arrow.svg";
+import "./countryheader.css";
+
+export default function CountryHeader() {
+  const list1 = [
+    { name: "المملكة المتحدة", flag: uk },
+    { name: "المانيا", flag: germany },
+    { name: "فرنسا", flag: france },
+    { name: "الصين", flag: china },
+    { name: "المملكة المتحدة", flag: uk },
+    { name: "المانيا", flag: germany },
+    { name: "فرنسا", flag: france },
+    { name: "الصين", flag: china },
+    { name: "المملكة المتحدة", flag: uk },
+    { name: "المانيا", flag: germany },
+    { name: "فرنسا", flag: france },
+    { name: "الصين", flag: china },
+  ];
+  return (
+    <div className="countryheader position-relative py-5">
+      <div className="position-absolute country-list w-100">
+        <ul className="list-unstyled d-flex justify-content-center align-items-center">
+          <li
+            className="country-list-links bg-white mx-2"
+            style={{ color: "#336279" }}
+          >
+            دولية
+          </li>
+          <Link href="" className="country-list-links text-white mx-2">
+            قارية
+          </Link>
+          <Link href="" className="country-list-links text-white mx-2">
+            عالمية
+          </Link>
+        </ul>
+      </div>
+      <div className="px-5">
+        <div className="row  gy-4">
+          {list1.map((country, index) => {
+            return (
+              <div key={index} className="col-md-3">
+                <div className="bg-white shadow-sm text-center">
+                  <Link href="">
+                    <div className="d-flex justify-content-between align-items-center p-3">
+                      <div className="country-flag d-flex justify-content-center align-items-center">
+                        <Image
+                          src={country.flag}
+                          width={60}
+                          height={40}
+                          alt="img-country"
+                        />
+                        <p className="text-black mb-0 ms-4 px-2 countryname">
+                          {" "}
+                          {country.name}
+                        </p>
+                      </div>
+                      <Image src={arrow} width={20} height={20} alt="arrow" />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className=" d-flex justify-content-center align-items-center pt-5">
+        <Link  href=' ' className=" countryshow text-white  px-5 py-3">عرض جميع الدول</Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/_Compontents/Navbar/Navbar.jsx
@@ -0,0 +1,121 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import logonavbar from "../../../assets/images/logonavbar.svg";
+import Image from "next/image";
+import { useEffect } from "react";
+
+import "./navbar.css";
+import { usePathname } from "next/navigation";
+export default function Navbar() {
+  const pathName = usePathname();
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg py-1  ">
+        <div className="container-fluid">
+          <Link className="navbar-brand px-lg-5" href="#">
+            <Image
+              className="logo"
+              src={logonavbar}
+              width={200}
+              height={100}
+              alt="Company logo"
+            />{" "}
+          </Link>
+
+          <button
+            className="navbar-toggler toggler-mobile"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className={`nav-link  text-white ${
+                    pathName === "/" ? "active" : ""
+                  }`}
+                  aria-current="page"
+                  href="/"
+                >
+                  البداية
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link  text-white" href="#">
+                  {" "}
+                  الدول
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link  text-white" href="#">
+                  {" "}
+                  العروض
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link  text-white" href="#">
+                  مركز المساعدة
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link  text-white" href="#">
+                  اتصل بنا
+                </Link>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="nav-link text-white"
+                  aria-label="Switch to English"
+                >
+                  English
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="nav-link text-white"
+                  aria-label="Switch currency to SAR"
+                >
+                  ر.س
+                </button>
+              </li>
+            </ul>
+
+            <div className="d-flex">
+              <ul className="navbar-nav ms-auto mb-2 mx-lg-5 mb-lg-0">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link  text-white"
+                    aria-current="page"
+                    href="/login"
+                  >
+                    الدخول
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link  text-white resgister px-3"
+                    href="#"
+                  >
+                    تسجيل الدخول
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
