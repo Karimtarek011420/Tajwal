0faae3f19e59eefcbd2d92cad01a6c957402d3cd
Karimtarek011420 <karimtarek011420@gmail.com>
2024-12-22 17:07:39 +0200

    y2

--- src/app/(MainCompoments)/accountInformation/page.jsx
+++ src/app/(MainCompoments)/accountInformation/page.jsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import "./accountinform.css";
 
 export default function accountInformation() {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user") || "{}")
+  );
+
   return (
     <>
       <div className="countryheader position-relative py-5">
@@ -15,6 +20,33 @@ export default function accountInformation() {
               معلومات الحساب
             </li>
           </ul>
+        </div>
+        <div className=" container py-5">
+          <div className=" row gy-4">
+            <div className=" col-md-3">
+              <div className=" cardinfo bg-info p-3">
+                <div>
+                  <h6> {user.first_name}</h6>
+                  <span dir="ltr">{user.phone_number}</span>
+                </div>
+                <hr
+                  style={{
+                    borderColor: "gray",
+                    marginTop: "20px",
+                    width: "100%",
+                  }}
+                />
+                <div>
+                  <ul className=" list-unstyled">
+                    <li>عنصر 1</li>
+                    <li>عنصر 2</li>
+                    <li>عنصر 3</li>
+                    <li>عنصر 4</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
