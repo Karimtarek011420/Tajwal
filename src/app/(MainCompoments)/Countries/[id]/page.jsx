0c4f098ecb7435d4724f99333ef7d0625e622b2c
Karimtarek011420 <karimtarek011420@gmail.com>
2024-12-24 16:20:11 +0200

    y3367228w7س472

--- src/app/(MainCompoments)/Countries/[id]/page.jsx
+++ src/app/(MainCompoments)/Countries/[id]/page.jsx
@@ -1,12 +1,10 @@
-
 import React from "react";
 import axios from "axios";
 import "./details.css";
+import Image from "next/image";
 
 export default async function DetialsCountry({ params }) {
   const { id } = await params; // الحصول على معرّف الدولة من الروت
-  console.log(id)
-
   const getCountryDetails = async () => {
     const { data } = await axios.get(
       `https://api.tajwal.co/api/v1/countrie/${id}`,
@@ -20,24 +18,32 @@ export default async function DetialsCountry({ params }) {
   };
 
   const countryDetails = await getCountryDetails();
-  console.log(" karim",countryDetails[0])
 
   return (
     <div className="countrydetials position-relative py-5">
-      <div className="position-absolute country-list w-100">
-        <ul className="list-unstyled d-flex justify-content-center align-items-center">
-          <li
-            className="country-list-links bg-white mx-2"
-            style={{ color: "#336279" }}
-          >
-            {countryDetails[0]?.title}
-          </li>
-        </ul>
-      </div>
-      <div className="country-content text-center">
-        <h1>{countryDetails[0]?.title}</h1>
-
-      </div>
+      {countryDetails.map((country) => {
+        return (
+          <div key={country.country_code}>
+            <div className="position-absolute country-list w-100">
+              <ul className="list-unstyled d-flex justify-content-center align-items-center">
+                <li
+                  className="country-list-links bg-white mx-2"
+                  style={{ color: "#336279" }}
+                >
+                  <Image
+                  src={country.image}
+                  width={35}
+                  height={25}
+                  alt={country.title}
+                  />
+                 <span className="mx-3"> {country.title}</span>
+                </li>
+              </ul>
+            </div>
+            <div className="country-content text-center"></div>
+          </div>
+        );
+      })}
     </div>
   );
 }
