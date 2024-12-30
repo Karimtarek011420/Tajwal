24636d89c459d6df8bfc27dc4555bc4d85c51b0b
Karimtarek011420 <karimtarek011420@gmail.com>
2024-12-30 11:36:14 +0200

    225221

--- src/app/(MainCompoments)/Purchase/page.jsx
+++ src/app/(MainCompoments)/Purchase/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import "./purchase.css";
 import { usePackage } from "@/app/_Compontents/PackageContext/PackageContext";
 import icon1 from "@/assets/images/Icon1dark.svg";
@@ -12,6 +12,8 @@ import Image from "next/image";
 
 export default function page() {
   const { selectedPackage } = usePackage();
+  const [quantity, setQuantity] = useState(1); // عداد الشرائح
+
   if (!selectedPackage) {
     return (
       <div className=" d-flex justify-content-center align-items-center py-5 selectedPackagereturn ">
@@ -21,6 +23,11 @@ export default function page() {
       </div>
     );
   }
+  const incrementQuantity = () => setQuantity(quantity + 1);
+  const decrementQuantity = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
+  const totalPrice = (selectedPackage.price * quantity).toFixed(2);
 
   return (
     <>
@@ -204,9 +211,52 @@ export default function page() {
               </div>
             </div>
             <div className=" col-md-4">
-                <div className="bg-white shadow-lg rounded-2">
-kk
+              <div className="bg-white shadow-lg rounded-2 mb-5 py-4 px-2">
+                <p className=" px-1   purchasepachagesum">المجموع</p>
+                <div className=" d-flex purchasepachage justify-content-between align-items-center my-1 px-2 py-2 ">
+                  <p className="my-0 ">
+                    شريحة -
+                    {selectedPackage.operator.coverages
+                      .map((coverage) => coverage.name)
+                      .join(", ")}
+                  </p>
+                  <div>
+                    <button onClick={incrementQuantity} className="btn">
+                      +
+                    </button>
+                    <span
+                      style={{
+                        fontSize: "8px",
+                        fontWeight: "600",
+                        backgroundColor: "#fff",
+                        padding: "6px 10px",
+                      }}
+                    >
+                      {quantity}
+                    </span>
+                    <button onClick={decrementQuantity} className="btn">
+                      -
+                    </button>
+                  </div>
+                  <p className="my-0"> {selectedPackage.price} ر.س</p>
                 </div>
+                <div className="  d-flex justify-content-end  ">
+                  <p className="purchasepachage px-5 py-1 my-2">
+                    المجموع : <span className=" me-5"> kkk</span>
+                  </p>
+                </div>
+                <div className="  d-flex justify-content-end  ">
+                  <p className="purchasepachage px-5 py-1 my-1">
+                    الخصم : <span className=" me-5"> kkk</span>
+                  </p>
+                </div>
+                <div className="  d-flex justify-content-end   ">
+                  <p className="purchasepachage px-5 py-1 my-1 boldall " >
+                    الإجمالى الكلى :<span className=" me-4"> kkk</span>
+                  </p>
+                </div>
+              </div>
+              <div className="bg-white shadow-lg rounded-2">kk</div>
             </div>
           </div>
         </div>
